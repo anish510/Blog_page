27f49e2ac3b91b1621dafb766b07366c25b6c59c
anish510 <anishjoshi510@gmail.com>
2023-06-15 22:06:54 +0545

    prototype blog done

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=10000000)
+    image = models.ImageField(
+        upload_to='static/images/demo/', blank=True)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
